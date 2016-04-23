7f3d606ca7328e7cb1f5bd56cc3d20b4a646059f
chris <s86610000@gmail.com>
2016-04-23 10:30:41 +0800

    加入專案檔案。

---
+++ DocFxForJenkinsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DocFxForJenkinsDemo.MvcApplication" Language="C#" %>
---
+++ DocFxForJenkinsDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocFxForJenkinsDemo.Startup))]
+namespace DocFxForJenkinsDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
